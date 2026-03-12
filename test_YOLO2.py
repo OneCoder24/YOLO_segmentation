3932780de0cf40991d88fc294851f44bc1a70b6a
Bunshi66 <slaven534228@gmail.com>
2026-03-12 15:26:21 +0300

    GPU training

--- test_YOLO2.py
+++ test_YOLO2.py
@@ -1,15 +1,25 @@
 from ultralytics import YOLO
+import torch
 
-# Load a model
-model = YOLO("yolo26n-seg.pt")  # load a pretrained segmentation model (recommended for training)
+if __name__ == '__main__':
+    # Проверка CUDA
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Using device: {device}")
 
-# Train the model on the Package Segmentation dataset
-results = model.train(data="package-seg.yaml", epochs=100, imgsz=640)
+    # Load a model
+    model = YOLO("yolo26n-seg.pt")  # load a pretrained segmentation model
 
-# Validate the model
-results = model.val()
+    # Train the model
+    results = model.train(
+        data="package-seg.yaml",
+        epochs=100,
+        imgsz=640,
+        device=0
+    )
 
-# Perform inference on an image
-results = model('rgb.png', conf=0.15)
+    # Validate the model
+    results = model.val()
 
-results[0].show()
+    # Perform inference on an image
+    results = model('rgb.png', conf=0.15, device=0)
+    results[0].show()
